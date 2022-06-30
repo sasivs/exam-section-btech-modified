184a9922cdb59195146929ba7e31573d7fa13d61
Sasi Bhushan VS <sasibhushansaladi@gmail.com>
2022-06-30 11:27:24 +0530

    Roll List Status and resources changes

--- SupExamDBRegistrations/views/Dec_upload_file.py
+++ SupExamDBRegistrations/views/Dec_upload_file.py
@@ -12,7 +12,7 @@ from ..forms import BacklogRegistrationForm, DBYBSAYASSelectionForm, FirstYearBa
           RegistrationsFinalizeEventForm, OpenElectiveRegistrationsForm
 from ..models import CurrentAcademicYear, RegistrationStatus, StudentBacklogs, StudentCancellation, \
     StudentGrades, StudentInfo, StudentMakeupBacklogsVsRegistrations,\
-     StudentRegistrations, StudentRegistrationsResource, ProgrammeModel, Subjects_Staging, \
+     StudentRegistrations,  ProgrammeModel, Subjects_Staging, \
          RollLists, Subjects, StudentRegistrations_Staging
 from tablib import Dataset
 from import_export.formats.base_formats import XLSX
