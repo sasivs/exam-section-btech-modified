05bc82c2d475730e9d123d239835409c22e7c5a7
Sasi Bhushan VS <sasibhushansaladi@gmail.com>
2022-07-07 16:46:06 +0530

    mark distribution done

--- superintendent/urls.py
+++ superintendent/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from superintendent.views import hod_assignment, IX_student
+from superintendent.views import HOD_assignment, IX_student, Marks_Distribution
 
 
 
 urlpatterns = [
-    path('HodAssignment', hod_assignment.hod_assignment, name='HodAssignment'),
+    path('HodAssignment', HOD_assignment.hod_assignment, name='HodAssignment'),
     path('IXGradeStudentsAdd', IX_student.ix_student_assignment, name='IXGradeStudentsAdd'),
+    path('AddMarkDistribution', Marks_Distribution.mark_distribution_add, name='AddMarkDistribution'),
+    path('MarkDistributionStatus', Marks_Distribution.mark_distribution_status, name='MarkDistributionStatus'),
+
 ]
