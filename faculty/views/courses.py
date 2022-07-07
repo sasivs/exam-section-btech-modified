92fc70f58a37332c3cfc0a0464af1a4a582c4aa5
Sasi Bhushan VS <sasibhushansaladi@gmail.com>
2022-07-07 14:39:20 +0530

    courses assigned

---
+++ faculty/views/courses.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.shortcuts import render
+from co_ordinator.models import FacultyAssignment
+from hod.models import Faculty_user 
+from SupExamDBRegistrations.user_access_test import grades_threshold_access
+
+
+@login_required(login_url="/login/")
+@user_passes_test(grades_threshold_access)
+def courses_assigned(request):
+    user = request.user
+    faculty = Faculty_user.objects.filter(User=user, RevokeDate__isnull=True)
+    subjects = FacultyAssignment.objects.filter(Faculty=faculty.Faculty, RegEventId__Status=1)
+    return render(request, 'faculty/CoursesAssigned.html', {'subjects':subjects})
