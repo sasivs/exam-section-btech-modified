fcc50f1cf6438f032bf3e6f44ce74fea8151ae7d
Interns <104820886+intern-bug@users.noreply.github.com>
2022-08-02 16:46:55 +0530

    Update Dec_upload_file.py

--- co_ordinator/views/Dec_upload_file.py
+++ co_ordinator/views/Dec_upload_file.py
@@ -3,10 +3,10 @@ from django.contrib.auth.decorators import login_required, user_passes_test
 from django.shortcuts import render
 from superintendent.user_access_test import registration_access
 from co_ordinator.forms import OpenElectiveRegistrationsForm
-from superintendent.models import RegistrationStatus,CycleCoordinator
-from co_ordinator.models import Subjects, StudentRegistrations_Staging
+from superintendent.models import BTRegistrationStatus,BTCycleCoordinator
+from co_ordinator.models import BTSubjects, BTStudentRegistrations_Staging
 from import_export.formats.base_formats import XLSX
-from hod.models import Coordinator
+from hod.models import BTCoordinator
 
 
 @login_required(login_url="/login/")
@@ -16,11 +16,11 @@ def dept_elective_regs_upload(request):
     groups = user.groups.all().values_list('name', flat=True)
     regIDs =None
     if 'Co-ordinator' in groups:
-        coordinator = Coordinator.objects.filter(User=user, RevokeDate__isnull=True).first()
-        regIDs = RegistrationStatus.objects.filter(Status=1, RegistrationStatus=1, Dept=coordinator.Dept, BYear=coordinator.BYear,Mode='R')
+        coordinator = BTCoordinator.objects.filter(User=user, RevokeDate__isnull=True).first()
+        regIDs = BTRegistrationStatus.objects.filter(Status=1, RegistrationStatus=1, Dept=coordinator.Dept, BYear=coordinator.BYear,Mode='R')
     elif 'Cycle-Co-ordinator' in groups:
-        cycle_cord = CycleCoordinator.objects.filter(User=user, RevokeDate__isnull=True).first()
-        regIDs = RegistrationStatus.objects.filter(Status=1, RegistrationStatus=1, Dept=cycle_cord.Cycle, BYear=1,Mode='R')
+        cycle_cord = BTCycleCoordinator.objects.filter(User=user, RevokeDate__isnull=True).first()
+        regIDs = BTRegistrationStatus.objects.filter(Status=1, RegistrationStatus=1, Dept=cycle_cord.Cycle, BYear=1,Mode='R')
     if regIDs:
         regIDs = [(row.AYear, row.ASem, row.BYear, row.BSem, row.Dept, row.Mode, row.Regulation) for row in regIDs]
     necessary_field_msg = False
@@ -51,12 +51,12 @@ def dept_elective_regs_upload(request):
             for chunk in file.chunks():
                 data += chunk
             dataset = XLSX().create_dataset(data)
-            currentRegEventId = RegistrationStatus.objects.filter(AYear=ayear,ASem=asem,BYear=byear,BSem=bsem,\
+            currentRegEventId = BTRegistrationStatus.objects.filter(AYear=ayear,ASem=asem,BYear=byear,BSem=bsem,\
                     Dept=dept,Mode=mode,Regulation=regulation)
             currentRegEventId = currentRegEventId[0].id
             for i in range(len(dataset)):
                 regNo = int(dataset[i][0])
-                reg = StudentRegistrations_Staging(RegNo=regNo, RegEventId=currentRegEventId,Mode=1,sub_id=subId)
+                reg = BTStudentRegistrations_Staging(RegNo=regNo, RegEventId=currentRegEventId,Mode=1,sub_id=subId)
                 reg.save()
             return render(request, 'co_ordinator/Dec_Regs_success.html')
         elif regId != '--Choose Event--':
@@ -68,10 +68,10 @@ def dept_elective_regs_upload(request):
             bsem = rom2int[strs[2]]
             regulation = int(strs[5])
             mode = strs[6]
-            currentRegEventId = RegistrationStatus.objects.filter(AYear=ayear,ASem=asem,BYear=byear,BSem=bsem,\
+            currentRegEventId = BTRegistrationStatus.objects.filter(AYear=ayear,ASem=asem,BYear=byear,BSem=bsem,\
                     Dept=dept,Mode=mode,Regulation=regulation)
             currentRegEventId = currentRegEventId[0].id
-            subjects = Subjects.objects.filter(RegEventId=currentRegEventId, Category='DEC')
+            subjects = BTSubjects.objects.filter(RegEventId=currentRegEventId, Category='DEC')
             subjects = [(sub.id,str(sub.SubCode)+" "+str(sub.SubName)) for sub in subjects]
             form = OpenElectiveRegistrationsForm(regIDs,subjects,data)
     else:
