57ba77d7833cc4af281f96c757a276d1d808ea47
Sasi Bhushan VS <sasibhushansaladi@gmail.com>
2022-08-08 16:56:53 +0530

    phone number ->textfield

--- MTExamStaffDB/models.py
+++ MTExamStaffDB/models.py
@@ -8,17 +8,17 @@ from MTco_ordinator.models import MTStudentRegistrations
 class MTStudentInfo(models.Model):
     RegNo = models.IntegerField()
    # RollNo = models.IntegerField()
-    Name = models.CharField(max_length=70)
+    Name = models.CharField(max_length=255)
     Regulation = models.IntegerField()
     Dept = models.IntegerField()
-    AdmissionYear=models.IntegerField()
+    AdmissionYear = models.IntegerField()
     Gender = models.CharField(max_length=10)
-    Category = models.CharField(max_length=20)
-    GuardianName = models.CharField(max_length=50)
-    Phone = models.IntegerField()
-    email = models.CharField(max_length=50)
-    Address1 = models.CharField(max_length=150)
-    Address2 = models.CharField(max_length=100, null=True)
+    Category = models.CharField(max_length=30)
+    GuardianName = models.CharField(max_length=255)
+    Phone = models.TextField()
+    email = models.TextField()
+    Address1 = models.TextField()
+    Address2 = models.TextField(null=True)
     
     class Meta:
         db_table = 'MTStudentInfo'
