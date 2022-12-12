f4982d31f572082215e8aa2ce98bcdfac186381d
Sasi Bhushan VS <sasibhushansaladi@gmail.com>
2022-12-12 16:59:03 +0530

    courses build script modified

--- build_courses.py
+++ build_courses.py
@@ -1,10 +1,21 @@
 from ADAUGDB.models import BTMarksDistribution
 import copy
 
+
+'''
+DB Query: 
+select bts."SubCode", bts."SubName", bts."Creditable", 
+bts."Credits", bts."Type", bts."Category", bts."OfferedBy",
+bts."DistributionRatio", bts."Distribution", bts."DistributionNames"
+, bts."PromoteThreshold", btrs."BYear", btrs."BSem", btrs."Dept", btrs."Regulation"
+from ("BTSubjects" btss join "BTMarkDistribution" btm on btss."MarkDistribution_id"=btm."id")bts
+join "BTRegistration_Status" btrs on bts."RegEventId_id"=btrs."id"
+'''
+
 def build_course_structure(file):
     import pandas as pd
     file = pd.read_excel(file)
-    file = file.drop_duplicates(subset=['SubCode', 'SubName', 'BYear', 'BSem', 'Dept', 'OfferedBy', 'Regulation', 'Creditable', 'Credits', 'Type', 'Category', 'DistributionRatio', 'MarkDistribution_id'])
+    file = file.drop_duplicates(subset=['SubCode', 'SubName', 'BYear', 'BSem', 'Dept', 'OfferedBy', 'Regulation', 'Creditable', 'Credits', 'Type', 'Category', 'DistributionRatio', 'Distribution', 'DistributionNames', 'PromoteThreshold'])
     file['lectures'] = 0
     file['tutorials'] = 0
     file['practicals'] = 0
@@ -15,28 +26,26 @@ def build_course_structure(file):
     # cs_file.index += 1
     pd.options.display.multi_sparse = True
     print(cs_file)
-    cs_file.to_csv("/home/examsection/Desktop/awsp_testing_v2/database_recreate/course_structure_from_2019.xlsx")
+    cs_file.to_excel("/home/examsection/Desktop/awsp_testing_v2/database_recreate/course_structure_from_2019.xlsx")
 
 def build_courses_file(file):
     import pandas as pd
     file = pd.read_excel(file)
-    file = file.drop_duplicates(subset=['SubCode', 'SubName', 'BYear', 'BSem', 'Dept', 'OfferedBy', 'Regulation', 'Creditable', 'Credits', 'Type', 'Category', 'DistributionRatio', 'MarkDistribution_id'])
+    file = file.drop_duplicates(subset=['SubCode', 'SubName', 'BYear', 'BSem', 'Dept', 'OfferedBy', 'Regulation', 'Creditable', 'Credits', 'Type', 'Category', 'DistributionRatio', 'Distribution', 'DistributionNames', 'PromoteThreshold'])
     file['lectures'] = 0
     file['tutorials'] = 0
     file['practicals'] = 0
     file['MarkDistribution'] = ''
-    mark_dis = BTMarksDistribution.objects.get(id=int(row['MarkDistribution_id']))
-    distribution_marks = mark_dis.Distribution.split(',')
-    marks = [row.split('+') for row in distribution_marks]
-    t_marks = [['100' for _ in range(len(row))] for row in marks]
-    t_marks = ['+'.join(mark) for mark in t_marks]
-    t_marks = ','.join(t_marks)
-    ncolumns = len(mark_dis.distributions())
-    if ncolumns == 4: dis_names = 'Minor-I'+','+'MID'+','+'Minor-II'+','+'END'
-    elif ncolumns == 2: dis_names = 'Internal'+','+'External'
-    distribution_string = t_marks+';'+str(mark_dis.PromoteThreshold)+';'+dis_names
-    # distribution_string_2 = '100+100+100+100;0+0+0+15;Minor-I+Mid+Minor-II+End'
     for _, row in file.iterrows():
+        distribution_marks = row['Distribution'].split(',')
+        ncolumns = len(distribution_marks)
+        marks = [row.split('+') for row in distribution_marks]
+        t_marks = [['100' for _ in range(len(row))] for row in marks]
+        t_marks = ['+'.join(mark) for mark in t_marks]
+        t_marks = ','.join(t_marks)
+        if ncolumns == 4: dis_names = 'Minor-I'+','+'MID'+','+'Minor-II'+','+'END'
+        elif ncolumns == 2: dis_names = 'Internal'+','+'External'
+        distribution_string = t_marks+';'+str(file['PromoteThreshold'])+';'+dis_names
         file.at[_,'MarkDistribution'] = distribution_string
 
     course_data = file[['SubCode', 'SubName', 'BYear', 'BSem', 'Dept', 'OfferedBy', 'Regulation', 'Creditable', 'Credits', 'Type', 'Category', 'lectures', 'tutorials', 'practicals', 'DistributionRatio', 'MarkDistribution']]
