b5be6dc98acf9f4d37fbe374b64a1e6f1b936ab8
Sasi Bhushan VS <sasibhushansaladi@gmail.com>
2022-12-15 09:44:50 +0530

    cancellation script

--- ADAUGDB/views/cancellations.py
+++ ADAUGDB/views/cancellations.py
@@ -103,3 +103,69 @@ def seat_cancellation_status(request):
         form = StudentCancellationStatusForm()
     return render(request, 'ADAUGDB/BTStudentCancellationStatus.html', {'form':form})
 
+
+def cancellation_script(file):
+    import pandas as pd
+    file = pd.read_csv(file)
+    errors=[]
+    for _,row in file.iterrows():
+        regno = int(row['RegNo'])
+        date = row['Date']
+        remark = row['remark']
+        studentInfo = BTStudentInfo.objects.filter(RegNo=regno).first()
+        if not studentInfo:
+            errors.append(regno)
+            continue
+        student = studentInfo.first()
+        droppedregular= BTDroppedRegularCourses.objects.filter(student_id=student.id)
+        notpromoted= BTNotPromoted.objects.filter(student_id=student.id)
+        notregistered =BTNotRegistered.objects.filter(Student_id=student.id)
+        rolls= BTRollLists.objects.filter(student_id=student.id)
+        regs =BTStudentRegistrations.objects.filter(student__student__RegNo=regno)
+        grades =BTStudentGrades.objects.filter(RegId_id__in = regs.values_list('id',flat =True))
+        marks = BTMarks.objects.filter(Registration__in = regs)
+        BTCancelledStudentInfo(id=student.id,Remarks=remark,CancelledDate=date,RegNo=student.RegNo,RollNo=student.RollNo,Name=student.Name,Regulation=student.Regulation,Dept=student.Dept,AdmissionYear=student.AdmissionYear,Gender=student.Gender,Category=student.Category,GuardianName=student.GuardianName,Phone=student.Phone,email=student.email,Address1=student.Address1,Address2=student.Address2,Cycle=student.Cycle).save()
+        for i in rolls:
+            i_dict = i.__dict__
+            i_dict.pop('_state')
+            BTCancelledRollLists.objects.create(**i_dict)
+        for i in notregistered:
+            i_dict = i.__dict__
+            i_dict.pop('_state')
+            BTCancelledNotRegistered.objects.create(**i_dict)
+        for i in regs:
+            i_dict = i.__dict__
+            i_dict.pop('_state')
+            BTCancelledStudentRegistrations.objects.create(**i_dict)
+        for i in droppedregular:
+            i_dict = i.__dict__
+            i_dict.pop('_state')
+            BTCancelledDroppedRegularCourses.objects.create(**i_dict)
+            
+        for i in marks:
+            i_dict = i.__dict__
+            i_dict.pop('_state')
+            BTCancelledMarks.objects.create(**i_dict)
+        for i in grades:
+            i_dict = i.__dict__
+            i_dict.pop('_state')
+            BTCancelledStudentGrades.objects.create(**i_dict)
+        for i in notpromoted:
+            i_dict = i.__dict__
+            i_dict.pop('_state')
+            BTCancelledNotPromoted.objects.create(**i_dict)
+        BTRollLists_Staging.objects.filter(student_id=student.id).delete()
+        BTMarks_Staging.objects.filter(Registration__in = regs).delete()
+        BTStudentGrades_Staging.objects.filter(RegId_id__in = regs.values_list('id',flat =True)).delete()
+        BTStudentRegistrations_Staging.objects.filter(student__student__RegNo=regno).delete()
+        rolls.delete()
+        marks.delete()
+        grades.delete()
+        regs.delete()
+        notregistered.delete()
+        notpromoted.delete()
+        droppedregular.delete()
+        studentInfo.delete()
+    print(errors)
+    print("These regno are invalid!!")
+    return "Completed!!!"
