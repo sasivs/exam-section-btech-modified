da8d52e21992de14ce0e89194d8d323afad2d029
Sasi Bhushan VS <sasibhushansaladi@gmail.com>
2022-12-17 11:51:02 +0530

    rolls verify script changes

--- Utils/rollLists_verify_B_2019.py
+++ Utils/rollLists_verify_B_2019.py
@@ -5,8 +5,10 @@ def verify_rollLists():
     f_name = 'RollList(PHYSICS_I_I_2019_1_3.0_B).xlsx'
     error_df = pd.read_excel(dprefix+'SampleRes.xlsx')
     gen_df = pd.read_excel(dprefix+f_name)
-    error_df = error_df[gen_df['_merge']=='left_only']
-    gen_df.drop(gen_df.loc[gen_df['RegNo'] in error_df['RegNo']], inplace=True)
+    error_df = error_df[error_df['_merge']=='left_only']
+    for _,row in error_df.iterrows():
+        gen_df.drop(gen_df.loc[gen_df['RegNo'] == row['RegNo']], inplace=True)
+        
     gen_df.to_excel(dprefix+f_name, index=False)
     print("done")
 
