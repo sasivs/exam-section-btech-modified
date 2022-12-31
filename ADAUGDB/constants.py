72e6291486ca3d3be9f1742531e20eb138c6e7e7
Sasi Bhushan VS <sasibhushansaladi@gmail.com>
2022-12-31 23:08:28 +0530

    REGULATIONS added

--- ADAUGDB/constants.py
+++ ADAUGDB/constants.py
@@ -3,3 +3,4 @@ YEARS = {1:'I',2:'II',3:'III',4:'IV'}
 SEMS = {1:'I',2:'II'}
 DEPT_DICT = {dept:ind+1 for ind, dept  in enumerate(DEPARTMENTS)}
 ROMAN_TO_INT = {'I':1,'II':2,'III':3,'IV':4}
+REGULATIONS = {1:'R15', 2:'R17', 3:'R18', 4:'R20'}
